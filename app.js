81167f55fae96116a187add3d0dc38bb7cd848e1
Alyssa Lew <alyssalew2@gmail.com>
2013-08-14 19:27:58 -0700

    fetching URLs lungtest.dyd & melanomatest.dyd

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 var express = require('express');
 var app = express();
+var http = require("http");
+var lungtest = {
+  host: 'lungtest.dyd.cancercommons.org',
+  port: 80,
+  path: '/index.html'
+};
+
+var melanomatest = {
+  host: 'melanomatest.dyd.cancercommons.org',
+  port: 80,
+  path: '/index.html'
+};
+
+http.get(lungtest, function(res) {
+  console.log("Yes, Lung Test!: " + res.statusCode);
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
+http.get(melanomatest, function(res) {
+  console.log("Yes, Melanoma Test!: " + res.statusCode);
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
+
 app.get('/', function(req, res){
   res.send('Hello DYD?');
 });
