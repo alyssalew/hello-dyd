01a00915dcac728f83e87f432e61b6b095c4ecf9
Alyssa Lew <alyssalew2@gmail.com>
2013-08-17 18:23:55 -0700

    add status code and error message

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var theLungStatusCode="";
+var theMelanomaStatusCode="";
+var theLungErrorMessage="";
+var theMelanomaErrorMessage="";
+
 var express = require('express');
 var app = express();
 var http = require("http");
@@ -15,20 +20,22 @@ var melanomatest = {
 
 http.get(lungtest, function(res) {
   console.log("Hello, Lung Test!: " + res.statusCode);
+  theLungStatusCode=res.statusCode;
 }).on('error', function(e) {
   console.log("Error on Lung Test: " + e.message);
+  theLungErrorMessage=e.message;
 });
 
 http.get(melanomatest, function(res) {
   console.log("Hello, Melanoma Test!: " + res.statusCode);
+  theMelanomaStatusCode=res.statusCode;
 }).on('error', function(e) {
   console.log("Error on Lung Test: " + e.message);
+  theMelanomaErrorMessage=e.message;
 });
 
-
-
 app.get('/', function(req, res){
-  res.send('<html> <head> <hl>Hello DYD?</hl> </head> <br> <body> <pre>Hello. Welcome to Hello DYD? This site will let you know whether the DYD services are availble.</pre> </body> </html>');
+  res.send('<html> <head> <hl>Hello DYD?</hl> </head> <br> <body> <pre>Hello. Welcome to Hello DYD? This site will let you know whether the DYD services are availble.</pre> <br> The Lung Status Code is <b>'+ theLungStatusCode +' </b> <br> The Lung Error Message: <b>'+ theLungErrorMessage +' </b> <br><br>The Melanoma Status Code is <b> '+ theMelanomaStatusCode+' </b><br> The Melanoma Error Message: <b>'+ theMelanomaErrorMessage+'</b></body> </html>');
 });
 
 app.listen(3000);
