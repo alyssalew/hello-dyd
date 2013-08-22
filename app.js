b5287c3617bab59d5ecf066538693f9e175b47d8
Alam Kasenally <dnamax@gmail.com>
2013-08-22 18:12:41 +0400

    1. Enabled HTTPS (so we get 200), 2. Continued Alyssa's great work and put all hosts, variables, etc into array

--- app.js
+++ app.js
@@ -1,59 +1,24 @@
 //These variables will be set every time the loop is run, so that we can show the latest run time
 var hours,minutes,seconds,day,month,year;
 var loopinterval = 60000; //microseconds: how often do we check the servers
-
-var mailOptionsLungTest = {
-   from: "cc.hello.dyd", // sender address
-   to: "alyssalew2@gmail.com", // list of receivers
-   subject: "Test-from lung", // Subject line
-   html: "<p>Some text</p>", // html body
-   generateTextFromHTML: true,
-}
-var mailOptionsMelanomaTest = {
-   from: "cc.hello.dyd", // sender address
-   to: "alyssalew2@gmail.com", // list of receivers
-   subject: "Test-from Melanoma", // Subject line
-   html: "<p>Some text</p>", // html body
-   generateTextFromHTML: true,
-}
-var mailOptionsLungLive ={
-   from: "cc.hello.dyd", // sender address
-   to: "alyssalew2@gmail.com", // list of receivers
-   subject: "Test-from lung", // Subject line
-   html: "<p>Some text</p>", // html body
-   generateTextFromHTML: true,
-}
-var mailOptionsMelanomaLive ={
-   from: "cc.hello.dyd", // sender address
-   to: "alyssalew2@gmail.com", // list of receivers
-   subject: "Test-from Melanoma", // Subject line
-   html: "<p>Some text</p>", // html body
-   generateTextFromHTML: true,
-}
-
 var express = require('express');
 var app = express();
 var http = require("http");
-var lungtest = {
-  host: 'lungtest.dyd.cancercommons.org',
-  port: 80,
-  path: '/index.html'
-};
-var melanomatest = {
-  host: 'melanomatest.dyd.cancercommons.org',
-  port: 80,
-  path: '/index.html'
-};
-var lunglive = {
-  host: 'lung.dyd.cancercommons.org',
-  port: 80,
-  path: '/index.html'
-};
-var melanomalive = {
-  host: 'melanoma.dyd.cancercommons.org',
-  port: 80,
-  path: '/index.html' 
-} 
+var https = require("https");
+var theWebPage="";
+//service:
+//[0]: friendly name
+//[1]: URL
+//[2]: port
+//[3]: path
+//[4]: HTTP status code
+//[5]: Error message
+var services = [
+  ['Lung Test','lungtest.dyd.cancercommons.org', 443, '/index.html'],
+  ['Melanoma Test','melanomatest.dyd.cancercommons.org', 443,'/index.html'],
+  ['Lung Live','lung.dyd.cancercommons.org', 443, '/index.html'],
+  ['Melanoma Live','melanoma.dyd.cancercommons.org', 443 ,'/index.html']
+];
 var nodemailer = require("nodemailer");
 var smtpTransport = nodemailer.createTransport("SMTP",{
    service: "Gmail",
@@ -63,26 +28,66 @@ var smtpTransport = nodemailer.createTransport("SMTP",{
    }
 });
 
-var LungTestStatusCode="";
-var MelanomaTestStatusCode="";
-var LungTestErrorMessage="";
-var MelanomaTestErrorMessage="";
+function addServiceText(theservice)
+{
+  theWebPage += "<br>";
+  theWebPage += "The " + theservice[0] + " status code is <b>" + theservice[4] + "</b>" ;
+  theWebPage += "<br>";
+  theWebPage += "The " + theservice[0] + " error message is " + theservice[5] ;
+  theWebPage += "<br>";
+}
+function addTimeStamp()
+{
+  theWebPage += "<br>";
+  theWebPage += '<br> Last test done at : ' +  hours + ':' + minutes + ':' + seconds + ' on ' +  month + '/' + day + '/' + year + '</body> </html>'
+}
 
-var LungLiveStatusCode="";
-var MelanomaLiveStatusCode="";
-var LungLiveErrorMessage="";
-var MelanomaLiveErrorMessage="";
 app.get('/', function(req, res){
-  res.send('<html> <head> <hl>Hello DYD?</hl> </head><br> <body> <pre>Hello. Welcome to Hello DYD? This site will let you know whether the DYD services are availble.</pre> <br> The Lung Status Code is <b>'+ LungTestStatusCode +' </b> <br> The Lung Error Message: <b>'+ LungTestErrorMessage +' </b> <br><br>The Melanoma Status Code is <b> '+ MelanomaTestStatusCode+' </b><br> The Melanoma Error Message: <b>'+ MelanomaTestErrorMessage+'</b><br><br> The Lung Status Code is <b> '+LungLiveStatusCode+'</b> <br> The Lung Error Message: <b>'+LungLiveErrorMessage+'</b> <br><br> The Melanoma Status Code is <b> '+MelanomaLiveStatusCode+'</b><br> The Melanoma Error Message:<b> '+MelanomaLiveErrorMessage+'</b><br> Last test done at : ' +  hours + ':' + minutes + ':' + seconds + ' on ' +  month + '/' + day + '/' + year + '</body> </html>');});
+  theWebPage = "<html> <head> <hl>Hello DYD?</hl> </head><br> <body> <pre>Hello. Welcome to Hello DYD? This site will let you know whether the DYD services are availble.</pre> <br>";
+  services.forEach(addServiceText);
+  addTimeStamp();
+  res.send(theWebPage);
+});
 
 app.listen(3000);
 console.log('Listening on port 3000');
 
 //loop starts here: it gets run every 60 seconds
-
-function checkHosts()
+function log(theservice)
 {
- console.log('Entered CheckHosts');
+  console.log("Hello, " + theservice[0] + "!");
+}
+
+function tickle(theservice)
+{
+  params = { host: theservice[1], 
+             port: theservice[2],
+ 	     path: theservice[3]
+           };
+  var mailOptions = {
+    from: "Hello DYD!", // sender address
+    to: "alyssalew2@gmail.com", // list of receivers
+    subject: "Alert from " + theservice[0], // Subject line
+    html: "<p>Service " + theservice[0] + " might be down! Oh no!", // html body
+    generateTextFromHTML: true,
+  }
+ 
+  https.get(params, function(res) {
+   theservice[4]=res.statusCode;
+ }).on('error', function(e) {
+     console.log("Error on  " + theservice[0] + ":" +  e.message);
+     theservice[4]=e.message;
+     smtpTransport.sendMail(mailOptions, function(error, response){
+      if(error){
+          console.log(error);
+      }else{
+          console.log("Message sent: " + response.message);
+      }
+   });
+ });
+}
+function setTime()
+{
  var currentTime = new Date();
  month = currentTime.getMonth() + 1;
  day = currentTime.getDate();
@@ -94,64 +99,15 @@ function checkHosts()
    minutes = '0' + minutes;
  }
  seconds = currentTime.getSeconds();
+}
 
+function checkHosts()
+{
+ console.log('Entered CheckHosts');
+ setTime();
+ services.forEach(log);
+ services.forEach(tickle);
 
- http.get(lungtest, function(res) {
-   console.log("Hello, Lung Test!: " + res.statusCode);
-   LungTestStatusCode=res.statusCode;
- }).on('error', function(e) {
-   console.log("Error on Lung Test: " + e.message);
-   LungTestErrorMessage=e.message;
-   smtpTransport.sendMail(mailOptionsLungTest, function(error, response){
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Message sent: " + response.message);
-    }
-   });
- });
- http.get(melanomatest, function(res) {
-   console.log("Hello, Melanoma Test!: " + res.statusCode);
-   MelanomaTestStatusCode=res.statusCode;
- }).on('error', function(e) {
-   console.log("Error on Melanoma Test: " + e.message);
-   MelanomaTestErrorMessage=e.message;
-   smtpTransport.sendMail(mailOptionsMelanomaTest, function(error, response){
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Message sent: " + response.message);
-    }
-   });
- });
- http.get(lunglive, function(res) {
-   console.log("Hello, Lung Live!: " + res.statusCode);
-   LungLiveStatusCode=res.statusCode;
- }).on('error', function(e) {
-   console.log("Error on Lung Live: " + e.message);
-   LungLiveErrorMessage=e.message;
-   smtpTransport.sendMail(mailOptionsLungLive, function(error, response){
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Message sent: " + response.message);
-    }
-   });
- });
- http.get(melanomalive, function(res) {
-   console.log("Hello, Melanoma Live!: " + res.statusCode);
-   MelanomaLiveStatusCode=res.statusCode;
- }).on('error', function(e) {
-   console.log("Error on Melanoma Live: " + e.message);
-   MelanomaLiveErrorMessage=e.message;
-   smtpTransport.sendMail(mailOptionsMelanomaLive, function(error, response){
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Message sent: " + response.message);
-    }
-   });
- });
  //Will queue the next call to checkHosts 60 seconds from now
  setTimeout((function() {
     checkHosts();
@@ -163,4 +119,5 @@ function checkHosts()
 setTimeout((function() {
   checkHosts();
 }), 1000); //setTimeOut
+
 
